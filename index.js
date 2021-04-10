c1ff994dab2e54dc4d0a5533edf00bf69a0bb784
Spotky1004 <spotky1004@gmail.com>
2021-04-10 15:21:26 +0900

    better map generate & blur & better shadow

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const canvas = document.getElementById("canvas");
 let c = canvas.getContext("2d");
 
 // define vars
-let mainLoop, player, mazeMap, tickSpeed;
+let mainLoop, player, mazeMap, tickSpeed, blured=false;
 
 function generateMaze(x=0) {
-    mazeMap.map[x] = Math.floor(Math.random()*2**mazeMap.yHeight).toString(2).padStart(mazeMap.yHeight, "0");
+    mazeMap.map[x] = (BigInt(Math.floor(Math.random()*1e15))*2n**BigInt(mazeMap.yHeight)/BigInt(1e15)).toString(2).padStart(mazeMap.yHeight, "0");
 }
 
 // game functions
@@ -40,7 +40,6 @@ function loop() {
 
     // canvas draw & player collision
     const unitLeng = canvas.height/mazeMap.yHeight;
-    c.lineWidth = unitLeng/4;
     c.lineCap = "round";
     c.shadowBlur = 10;
     for (let x = Math.floor(mazeMap.x), xl = x+mazeMap.yHeight*4; x < xl; x++) {
@@ -50,6 +49,7 @@ function loop() {
         for (let y = 0; y < mazeMap.yHeight; y++) {
             const d = Number(mapUnit[y]);
             c.beginPath();
+            c.lineWidth = unitLeng/4;
             c.strokeStyle = `hsl(${(x+y)*Math.sqrt(10)%360}, 80%, 50%)`;
             c.shadowColor = c.strokeStyle;
             c.moveTo(unitLeng*(x-mazeMap.x+1), unitLeng*(y+1));
@@ -59,17 +59,39 @@ function loop() {
 
         if (x%5 === 0) {
             const textToDraw = x.toString();
-            c.font = `${100/mazeMap.yHeight}vh Arial`;
-            c.fillStyle = `hsla(${(x*Math.sqrt(10)+180)%360}, 80%, 50%, 0.5)`;
+            c.beginPath();
+            c.lineWidth = 0.3;
+            c.font = `bold ${100/mazeMap.yHeight}vh Arial`;
+            c.fillStyle = `hsla(${(x*Math.sqrt(10)+180)%360}, 60%, 60%, 0.5)`;
             c.shadowColor = c.fillStyle;
-            c.fillText(textToDraw, unitLeng*(x-mazeMap.x+0.5)-c.measureText(textToDraw).width/2, canvas.height-50); 
+            const t = [textToDraw, unitLeng*(x-mazeMap.x+0.5)-c.measureText(textToDraw).width/2, canvas.height-50];
+            c.fillText(...t);
+            c.strokeText(...t);
         }
     }
+
+    // player
+    const playerSize = 0.8;
+    const playerSpeed = Math.max(0.1, mazeMap.yHeight*mazeMap.speed/100);
+    
 }
 
 startLoop = (t) => mainLoop = mainLoop ?? setInterval(loop, t);
 
-startGame({yHeight: 30, acceleration: 0.1, tick: 30});
+startGame({yHeight: 15, acceleration: 0.1, tick: 30});
 
 // keypress
+let keyPressed = {};
+document.onkeydown = (e) => keyPressed[e.key] = true;
+document.onkeyup = (e) => keyPressed[e.key] = false;
 
+// window.blur
+window.onblur = () => {
+    blured = 1;
+    keyPressed = {};
+    mainLoop = clearInterval(mainLoop);
+}
+window.onfocus = () => {
+    blured = 0;
+    mainLoop = startLoop(tickSpeed);
+}
